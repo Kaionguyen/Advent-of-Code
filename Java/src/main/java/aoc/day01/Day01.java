0f3b42cbab35197b7860e82a7683bdd6cd688e7a
Kyle Nguyen <kylenguyencs@gmail.com>
2024-12-02 13:50:39 -0800

    Add: Unit test and solution for part 1

--- Java/src/main/java/aoc/day01/Day01.java
+++ Java/src/main/java/aoc/day01/Day01.java
@@ -4,13 +4,32 @@ import aoc.Day;
 import aoc.Utils;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class Day01 implements Day {
 
     @Override
     public String part1(String input) {
-        List<String> lines = Utils.splitLines(input);
-        return lines.isEmpty() ? "" : lines.get(0);
+        String[] lines = input.split("\n");
+        
+        int[] list1 = new int[lines.length];
+        int[] list2 = new int[lines.length];
+        
+        for (int i = 0; i < lines.length; i++) {
+            String[] pair = lines[i].split("\\s+");
+            list1[i] = Integer.parseInt(pair[0]);
+            list2[i] = Integer.parseInt(pair[1]);
+        }
+
+        Arrays.sort(list1);
+        Arrays.sort(list2);
+
+        int runningSum = 0;
+        for (int i = 0; i < list1.length; i++) {
+            runningSum += Math.abs(list2[i] - list1[i]);
+        }
+
+        return String.valueOf(runningSum);
     }
 
     @Override
--- Java/src/test/java/aoc/day01/Day01Test.java
+++ Java/src/test/java/aoc/day01/Day01Test.java
@@ -13,13 +13,19 @@ public class Day01Test {
     @Test
     public void testPart1(){
         // Given
-        String input = "test";
+        String input = "3   4\n" +
+                       "4   3\n" +
+                       "2   5\n" +
+                       "1   3\n" +
+                       "3   9\n" +
+                       "3   3";
+
 
         // When
         String result = new Day01().part1(input);
 
         // Then
-        assertEquals(input, result);
+        assertEquals("11", result);
     }
 
     @Test
